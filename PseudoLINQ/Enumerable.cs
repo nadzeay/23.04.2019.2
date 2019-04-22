4d5b2c9d758e1a2f3431391c5585746ed0fc439a
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2019-04-22 18:38:56 +0300

    Add project files.

---
+++ PseudoLINQ/Enumerable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PseudoEnumerable
+{
+    public static class Enumerable
+    {
+        /// <summary>
+        /// Filters a sequence of values based on a predicate.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{TSource}"/> to filter.</param>
+        /// <param name="predicate">A function to test each source element for a condition</param>
+        /// <returns>
+        ///     An <see cref="IEnumerable{TSource}"/> that contains elements from the input
+        ///     sequence that satisfy the condition.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="predicate"/> is null.</exception>
+
+        public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
+            Func<TSource> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Transforms each element of a sequence into a new form.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TResult">The type of the value returned by transformer.</typeparam>
+        /// <param name="source">A sequence of values to invoke a transform function on.</param>
+        /// <param name="transformer">A transform function to apply to each source element.</param>
+        /// <returns>
+        ///     An <see cref="IEnumerable{TResult}"/> whose elements are the result of
+        ///     invoking the transform function on each element of source.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="transformer"/> is null.</exception>
+        public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, TResult> transformer)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending order according to a key.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by key.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="key">A function to extract a key from an element.</param>
+        /// <returns>
+        ///     An <see cref="IEnumerable{TSource}"/> whose elements are sorted according to a key.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="key"/> is null.</exception>
+
+        public static IEnumerable<TSource> SortByDescending<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> key)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending order according by using a specified comparer for a key .
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by key.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="key">A function to extract a key from an element.</param>
+        /// <param name="comparer">An <see cref="IComparer{T}"/> to compare keys.</param>
+        /// <returns>
+        ///     An <see cref="IEnumerable{TSource}"/> whose elements are sorted according to a key.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="key"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="comparer"/> is null.</exception>
+        public static IEnumerable<TSource> SortBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> key, IComparer<TKey> comparer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
